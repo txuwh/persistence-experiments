93e32f508f882ec809a4da4e45b4bcfade9fc65f
Yury Yudin <yury.yudin@adcubum.com>
2020-04-03 17:53:38 +0200

    F findById should return Optional.

--- src/main/java/com/adcubum/persistence/BusinessHistoryObjectCRUDRepository.java
+++ src/main/java/com/adcubum/persistence/BusinessHistoryObjectCRUDRepository.java
@@ -4,12 +4,13 @@ import com.adcubum.persistence.entity.PartnerState;
 import com.adcubum.persistence.entity.State;
 
 import java.util.Date;
+import java.util.Optional;
 
 public interface BusinessHistoryObjectCRUDRepository<T extends State> {
 
     void create(T state);
 
-    T findById(String id, Date keyDate);
+    Optional<T> findById(String id, Date keyDate);
 
     void update(T state);
 
--- src/main/java/com/adcubum/persistence/repo/PartnerRepo.java
+++ src/main/java/com/adcubum/persistence/repo/PartnerRepo.java
@@ -3,5 +3,9 @@ package com.adcubum.persistence.repo;
 import com.adcubum.persistence.entity.Partner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PartnerRepo extends CrudRepository<Partner, String> {
+
+    List<Partner> findPartnerByBirthName(String birthName);
 }
